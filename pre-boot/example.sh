ebabd3738bd67fb21f3acd823e58d42db1d9c386
Liam Denston <email@uktsliam.info>
2018-03-03 13:15:13 +0000

    Update and rename example.txt to example.sh

---
+++ pre-boot/example.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#########################################################
+#
+# Pre-Boot Example
+#
+# Note: If your file does not load, we recommend you use `dos2unix -R /home/XenoPanel/scripts`
+# 
+#########################################################
+#
+# XenoPanel will call {game}.sh on pre-boot to allow for custom configuration.
+#
+# $1 = Run Type (install/existing)
+# $2 = Server ID
+# $3 = Server Ownership Username
+# $4 = Server IP Address
+# $5 = Server Port
+# $6 = Server Slots
+# $7 = Server Gametype (minecraft/source/voice)
+# $8 = Server Game
+# $9 = Launch (Example: ./srcds_linux)
+#
+#########################################################
